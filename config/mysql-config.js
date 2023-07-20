4d0abf43a98611693e11dccf062e71436659a4dd
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-07-20 11:51:39 +0100

    create mysqlDB configuration

--- config/mysql-config.js
+++ config/mysql-config.js
@@ -1,1 +1,25 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
+
+require("dotenv").config();
+
+const { DB_USERNAME, DB_PASSWORD } = process.env;
+
+// const DB_USERNAME = process.env.DB_USERNAME;
+// const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: "express_mysql",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("DB connected");
+  }
+});
+
+module.exports = connection;
