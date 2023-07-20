5062f6c24a9f5fd6bdf290ca21c1db6e80f7b379
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-07-20 15:05:41 +0100

    get all comments

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,36 @@
-var express = require('express');
+const express = require("express");
 var router = express.Router();
 
+const mysql = require("../config/mysql-config");
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
+
+router.get("/comments", (req, res, next) => {
+  const getCommentsQuery = "SELECT * FROM `comments`";
+
+  mysql.query(getCommentsQuery, (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+/**
+ Can also use
+  router.get("/comments", async (req, res, next) => {
+  try {
+    const getCommentsQuery = "SELECT * FROM `comments`";
+    const results = await mysql.query(getCommentsQuery);
+    res.send(results);
+  } catch (err) {
+    res.send(err);
+  }
+});
+ */
 
 module.exports = router;
